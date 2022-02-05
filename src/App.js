873cc0978816ae5fc2a76814fb0c9a2385390821
Ivan Branimir Skoric <ib.skoric@outlook.com>
2022-02-05 19:24:40 +0000

    Add clear button functionality

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
       <section className='container'>
         <h3>{people.length} Birthdays today.</h3>
         <List people={people} />
-        <button onClick={() => console.log('clicked')}>Clear all</button>
+        <button onClick={() => setPeople([])}>Clear all</button>
       </section>
     </main>
   );
