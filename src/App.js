0127f36bbae2723cee067b3b4f1c5e660f92c88e
Ivan Branimir Skoric <ib.skoric@outlook.com>
2022-02-05 19:15:24 +0000

    Install node modules, initial setup for App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
-function App() {
-  return <h2>reminder project setup</h2>;
+
+const App = () => {
+  return (
+    <main>
+      <section className='container'>
+        <h3>0 Birthdays today.</h3>
+        <List />
+        <button onClick={() => console.log('clicked')}>Clear all</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
