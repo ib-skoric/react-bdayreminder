372d837f66c587ff16a891d8ce8266d8056942a4
Ivan Branimir Skoric <ib.skoric@outlook.com>
2022-02-05 19:23:53 +0000

    Add functionality for the list component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import data from './data';
 import List from './List';
 
 const App = () => {
+
+  const [people, setPeople] = useState(data);
+
   return (
     <main>
       <section className='container'>
-        <h3>0 Birthdays today.</h3>
-        <List />
+        <h3>{people.length} Birthdays today.</h3>
+        <List people={people} />
         <button onClick={() => console.log('clicked')}>Clear all</button>
       </section>
     </main>
